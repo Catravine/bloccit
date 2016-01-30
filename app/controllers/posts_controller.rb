ea85357284457ee2add5c9aa8b99a65764fc9762
Caroline Courtney <caroline@carolinecourtney.com>
2016-01-30 03:06:42 -0500

    Assignment 31 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,27 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    censor
   end
 
   def show
+    censor
   end
 
   def new
+    censor
   end
 
   def edit
+    censor
+  end
+
+  def censor
+    @posts.each_with_index do |post, index|
+      if (index % 5 == 0)
+        post.update title: "SPAM"
+        post.save!
+      end
+    end
   end
 end
