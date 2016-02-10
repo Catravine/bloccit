21cf958612fb3a03fc046adac15d055e71f2393d
Caroline Courtney <caroline@carolinecourtney.com>
2016-02-10 05:03:03 -0500

    Checkpoint 42 Labels: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -19,6 +19,7 @@ before_action :authorize_user, except: [:index, :show]
     @topic = Topic.new(topic_params)
 
     if @topic.save
+      @topic.labels = Label.update_labels(params[:topic][:labels])
       redirect_to @topic, notice: "Topic was saved successfully."
     else
       flash.now[:alert] = "Error creating topic. Please try again."
@@ -36,6 +37,7 @@ before_action :authorize_user, except: [:index, :show]
     @topic.assign_attributes(topic_params)
 
     if @topic.save
+      @topic.labels = Label.update_labels(params[:topic][:labels])
       flash[:notice] = "Topic was updated."
       redirect_to @topic
     else
