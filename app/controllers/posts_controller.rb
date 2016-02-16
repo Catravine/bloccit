481131cc5e2897af4b524142c03c3d160b66f92f
Caroline Courtney <caroline@carolinecourtney.com>
2016-02-16 04:29:19 -0500

    Checkpoint 45 User Profiles: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
-  before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
+  skip_before_filter :require_sign_in, only: :show
 
   def show
     @post = Post.find(params[:id])
