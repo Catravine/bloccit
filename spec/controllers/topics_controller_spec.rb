82e66a3c9317a8ea733836542e3181ddcc7e6b95
Caroline Courtney <caroline@carolinecourtney.com>
2016-02-18 00:33:53 -0500

    Checkpoint-45-profiles-2

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq(Topic.all)
       end
     end
 
@@ -89,7 +89,7 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq(Topic.all)
       end
     end
 
@@ -163,7 +163,7 @@ RSpec.describe TopicsController, type: :controller do
 
       it "assigns Topic.all to topic" do
         get :index
-        expect(assigns(:topics)).to eq([my_topic])
+        expect(assigns(:topics)).to eq(Topic.all)
       end
     end
 
