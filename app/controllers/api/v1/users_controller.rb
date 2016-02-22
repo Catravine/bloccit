e2c299ae4c995c5c6790a48be0a8481e461d76a8
Caroline Courtney <caroline@carolinecourtney.com>
2016-02-22 16:52:00 -0500

    Topics work, success in users works

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  before_action :authenticate_user
-  before_action :authorize_user
+
+  skip_before_action :require_sign_in, only: [:index, :show]
+  skip_before_action :authorize_user, only: [:index, :show]
 
   def show
     user = User.find(params[:id])
